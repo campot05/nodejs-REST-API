679906775455687d0fb8491d1e830cc93b1c12f4
Danylo Trutniev <daniil.spectr@gmail.com>
2023-04-13 10:33:37 +0300

    add mongoose and dotenv

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const { DB_HOST } = process.env;
+// mongoose.set('stringQuery', false);
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log('Database connection successful');
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log('Server running. Use our API on port: 3000');
+// });
