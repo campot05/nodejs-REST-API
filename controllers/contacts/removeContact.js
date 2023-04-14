68168c06e5a3275c85dcd60d1522dfb7b1e78d78
Danylo Trutniev <daniil.spectr@gmail.com>
2023-04-14 11:05:34 +0300

    clear console

--- controllers/contacts/removeContact.js
+++ controllers/contacts/removeContact.js
@@ -3,10 +3,8 @@ const Contact = require('../../models/contacts');
 const { RequestError } = require('../../helpers');
 
 const removeContact = async (req, res) => {
-  console.log(req.params);
   const { contactId } = req.params;
   const result = await Contact.findByIdAndRemove(contactId);
-  console.log(result);
   if (!result) {
     throw RequestError(404, 'Not found');
   }
