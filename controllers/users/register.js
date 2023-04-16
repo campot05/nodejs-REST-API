5d178afac847cc413304ec0b71d5f494e68403a8
Danylo Trutniev <daniil.spectr@gmail.com>
2023-04-16 11:34:35 +0300

    add hash password

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,15 +1,19 @@
+const bcrypt = require('bcrypt');
+
 const User = require('../../models/user');
 const RequestError = require('../../helpers/RequestError');
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user) {
     throw RequestError(409, 'Email in use');
   }
 
-  const newUser = await User.create(req.body);
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
 
   res.status(201).json({
     user: {
